2ab893a53c83612775438d940d8bfa790bab5494
Rick Companje <rick@companje.nl>
2022-10-03 11:05:18 +0200

    Update csv2xlsx.py

--- csv2xlsx.py
+++ csv2xlsx.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 
-# source: https://stackoverflow.com/questions/17684610/python-convert-csv-to-xlsx
-
 import csv,sys,json
-
-# print("csv2xlsx start");
-# print(sys.version)
-# print(sys.executable)
-# print(sys.path)
-
 from xlsxwriter.workbook import Workbook
-
 from sys import argv
 from tqdm import tqdm
-
 
 delimiter=";"
 encoding="utf-8"
@@ -24,29 +14,14 @@ input_filename = argv[1]
 output_filename = argv[2] 
 settings_filename = argv[3] if len(argv)>3 else None
 
-# sys.exit(0)
-
-
 # load settings if supplied
 settings = json.load(open(settings_filename)) if settings_filename else None
 
-# if settings and "strings_to_numbers" in settings:
-#     workbook = Workbook(output_filename, settings["strings_to_numbers"])
-# else:
 workbook = Workbook(output_filename, {"strings_to_numbers": False, "strings_to_urls": False})
 
 worksheet = workbook.add_worksheet()
-# rows = [ row for row in csv.reader(open(input_filename, 'rt', encoding=encoding), delimiter=delimiter) ]
 rows = [ row for row in csv.reader(open(input_filename, 'rt', encoding=encoding), delimiter=delimiter) ]
 for r, row in enumerate(rows): #tqdm(rows)):
-
-    # add extra columns from settings? 'add_columns'
-
-    # print(row.values())
-    # sys.exit()
-
-    # GUID = row["GUID"]
-    # row.append(f"=HYPERLINK(\"https://google.com\",\"link\")")
 
     for c, col in enumerate(row):
         worksheet.write(r, c, col)
@@ -74,8 +49,4 @@ if settings:
 
             worksheet.set_column(c, c, col_width, cell_format)
 
-#############
-
 workbook.close()
-
-# print("csv2xlsx done");
